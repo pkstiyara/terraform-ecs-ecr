f09081e75d51e86bd623194fc2ac96313e23b626
pkstiyara <pkstiyara@gmail.com>
2023-02-17 12:34:46 +0530

    providers

--- providers.tf
+++ providers.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     aws = {
       source = "hashicorp/aws"
-      version = "4.55.0"
+      version = ">= 2.48.0, >= 3.40.0, >= 3.73.0, >= 4.20.0, >= 4.22.0, >= 4.53.0"
     }
   }
 }

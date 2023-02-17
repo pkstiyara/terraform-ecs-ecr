1fdc84d57876a9717ea284321cba80425822da3d
pkstiyara <pkstiyara@gmail.com>
2023-02-17 14:08:18 +0530

    changes in alb

--- aws_alb.tf
+++ aws_alb.tf
@@ -2,4 +2,9 @@ module "alb" {
   source  = "terraform-aws-modules/alb/aws"
   version = "6.4.0"
   # insert the 4 required variables here
+  name = "my-alb"
+
+  load_balancer_type = "application"
+
+  vpc_id             = module.vpc.vpc_id
 }
---
+++ outputs.tf
@@ -0,0 +1,4 @@
+output "vpc_id" {
+    value = aws_vpc.main.vpc_id
+  
+}
